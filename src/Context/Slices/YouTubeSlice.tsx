77e6d2524c050010c0ec4db678c9d672ddfb9f55
Diego Soro <dieee.93@gmail.com>
2023-07-09 18:59:18 -0300

    [fix]Problema con los Tipos de reac-colorful dep

--- src/Context/Slices/YouTubeSlice.tsx
+++ src/Context/Slices/YouTubeSlice.tsx
@@ -1,14 +1,14 @@
 import { youtubeApi } from '@/Api/YoutubeApi'
 import withResults from '@/Mocks/Youtube/with-results.json'
 
-const key = 'AIzaSyBzKhBag6vP6w0-YIdI9tIeBqMmsfyUjH8'
+const apiKey = import.meta.env.VITE_YOUTUBE_APIKEY as string
 
 const movie = withResults
 
 const extendedApiSlice = youtubeApi.injectEndpoints({
   endpoints: (builder) => ({
     getVideoThumbnail: builder.query<typeof movie, string>({
-      query: (videoId: string) => `videos?part=snippet&id=${videoId}&key=${key}`,
+      query: (videoId: string) => `videos?part=snippet&id=${videoId}&key=${apiKey}`,
       providesTags: ['YouTubeVideo']
     })
   })
